46146eef55ad54b29118691fbb2b225bf414a49f
ukki <iu.cashirskaia@yandex.ru>
2024-10-03 16:39:06 +0300

    рефакторирнг - index.php подчищен от лишних функций - создан отдельный класс для работы с бд

---
+++ dataClass.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace data;
+
+class dataClass
+{
+    function insertIntoOrders($arFromDocument)
+    {
+        // Подключение к базе данных
+        $connection = mysqli_connect("localhost:3307", "root", "123456", "try");
+
+        mysqli_close($connection); // Закрываем соединение с базой данных
+    }
+
+    function setOrdersData($arFromDocument)
+    {
+        // Подключение к базе данных
+        $connection = mysqli_connect("localhost:3307", "root", "123456", "try");
+        foreach ($arFromDocument as $Res) {
+            $id_good = (int)trim($Res[0]);
+            $idCustomer = (int)trim($Res[1]);
+            $comment = trim($Res[2]);
+            $query = "INSERT INTO orders (customerId, goods_id, comment) VALUES ($id_good, $idCustomer, '$comment')";
+            $result = mysqli_query($connection, $query);
+        }
+        mysqli_close($connection); // Закрываем соединение с базой данных
+        return $result;
+    }
+
+    function getClientsData()
+    {
+        // Подключение к базе данных
+        $connection = mysqli_connect("localhost:3307", "root", "123456", "try");
+        //todo реализовать
+        mysqli_close($connection); // Закрываем соединение с базой данных
+    }
+
+    function getMerchendise()
+    {
+        // Подключение к базе данных
+        $connection = mysqli_connect("localhost:3307", "root", "123456", "try");
+        //todo реализовать
+        mysqli_close($connection); // Закрываем соединение с базой данных
+    }
+}
--- www/index.php
+++ www/index.php
@@ -1,34 +1,30 @@
 <?php
-//todo вынести в отделюную функцию в отдельном файле
-$arRow = (str_getcsv(file_get_contents('doc_for_test.csv'), ';'));
 
-for ($i = 0; $i < count($arRow) - 1; $i += 3) {
-    $arRes[] = [$arRow[$i], $arRow[$i + 1], $arRow[$i + 2]];
-}
-unset($arRes[0]);
+declare(strict_types=1);
 
-// Подключение к базе данных
-$conn = mysqli_connect("localhost:3307", "root", "123456", "try");
+require_once('/home/ukki/Documents/TestTask_AO_Servise_RemoteTrade_/dataClass.php');
 
-// Запрос к таблице 'users' с условием 'id = 1'
-foreach ($arRes as $Res) {
-    $id_good = (int)trim($Res[0]);
-    $idCustomer = (int)trim($Res[1]);
-    $comment = trim($Res[2]);
-    $query = "INSERT INTO orders (customerId, goods_id, comment) VALUES ($id_good, $idCustomer, '$comment')";
-    $result = mysqli_query($conn, $query);
-}
+use data\dataClass;
 
+function getDataFromFile() // получает данные из файла и записывает их в массив массивов
+{
+    //todo вынести в отделюную функцию в отдельном файле
+    $arRow = (str_getcsv(file_get_contents('/home/ukki/Documents/TestTask_AO_Servise_RemoteTrade_/doc_for_test.csv'), ';'));
 
-$re = mysqli_query($conn, "SELECT * FROM orders");
-// echo $re;
-if ($re) {
-    // Обработка результата запроса
-    while ($row = mysqli_fetch_assoc($re)) {
-        echo $row['comment'] . ' ' . $row['customerId'] . ' ' . $row['goods_id'] . '<br />'; // Выводим значение поля 'name' из результата
+    for ($i = 0; $i < count($arRow) - 1; $i += 3) {
+        $arRes[] = [$arRow[$i], $arRow[$i + 1], $arRow[$i + 2]];
     }
-} else {
-    echo "Ошибка запроса: " . mysqli_error($conn);
+    unset($arRes[0]); //удаление первой строки в которой просто описание столбцов
+    return $arRes;
 }
 
-mysqli_close($conn); // Закрываем соединение с базой данных
+$dataObject = new dataClass;
+$dataObject->setOrdersData(getDataFromFile());
+
+$connection = mysqli_connect("localhost:3307", "root", "123456", "try");
+$query = "SELECT * FROM orders";
+$result = mysqli_query($connection, $query);
+foreach ($result as $re) {
+    echo $re['id'] . ' ' . $re['customerId'] . ' ' . $re['goods_id'] . ' ' . $re['comment'] . '<br />';
+}
+mysqli_close($connection); // Закрываем соединение с базой данных
